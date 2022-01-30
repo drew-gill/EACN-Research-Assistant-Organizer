bc5e52ba7e4d63d1d734ee2ca12af144d57c681a
Drew <32966255+drew-gill@users.noreply.github.com>
2022-01-30 03:26:46 -0500

    Set up basic slack bot functionality
    Utilize  https://us-central1-research-assistant-organizer.cloudfunctions.net/slack/events for the Slack Bolt API

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,32 @@
+'use strict'
 const functions = require('firebase-functions');
+const config = functions.config();
 
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-
-app.get('*', (req, res) => {
-  res.send("Hello from the API");
+const { App, ExpressReceiver } = require('@slack/bolt');
+const expressReceiver = new ExpressReceiver({
+    signingSecret: config.slack.signing_secret,
+    endpoints: '/events',
+    processBeforeResponse: true,
+});
+const app = new App({
+    receiver: expressReceiver,
+    token: config.slack.bot_token,
+    processBeforeResponse: true,
 });
 
-app.post('*', (req, res) => {
-    bodyParser.json(req);
+// Global error handler
+app.error(console.log);
 
-    res.send(req.body.challenge);
+// Handle `/echo` command invocations
+app.command('/echo-from-firebase', async ({ command, ack, say }) => {
+    // Acknowledge command request
+    await ack();
+
+    // Requires:
+    // Add chat:write scope + invite the bot user to the channel you run this command
+    // Add chat:write.public + run this command in a public channel
+    await say(`You said "${command.text}"`);
 });
 
-exports.api = functions.https.onRequest(app);
+// https://{your domain}.cloudfunctions.net/slack/events
+exports.slack = functions.https.onRequest(expressReceiver.app);
